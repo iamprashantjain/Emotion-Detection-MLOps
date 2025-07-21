793aef102927db3be7dae47906afe1d77cd01612
iamprashant <iamprashantjain2601@gmail.com>
2025-07-21 21:47:57 +0530

    model promotion code added in utils

--- flask_app/app.py
+++ flask_app/app.py
@@ -75,8 +75,8 @@ os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
-repo_owner = "campusx-official"
-repo_name = "mlops-mini-project"
+repo_owner = "iamprashantjain"
+repo_name = "Emotion-Detection-MLOps"
 
 # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
